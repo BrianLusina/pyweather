51053a4352c5b01c70439e335f1b4648b5f25f32
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2023-12-07 15:10:10 +0300

    feat(utils): get weather api key

--- pyweather/utils.py
+++ pyweather/utils.py
@@ -1,39 +1,25 @@
 """
 Utility functions
 """
-from typing import Set
+from pathlib import Path
+from configparser import ConfigParser
+
+_SECRETS_FILE = Path(__file__).parent / "secrets.ini"
 
 
-def join_guessed_letters(guessed_letters: Set[str]) -> str:
+def get_api_key(secrets_file: Path = _SECRETS_FILE) -> str:
     """
-    Joins the guessed letters and returns a string
+    Retrieves the API key from the secret file.
+
+    Expects a configuration file named "secrets.ini" with structure:
+
+        [openweather]
+        api_key=<YOUR-OPENWEATHER-API-KEY>
     Args:
-        guessed_letters (set): guessed letters
+        secrets_file (Path): path to a secret file.
     Returns:
-        str: joined letters as a string separated by a space delimiter
+        str: API Key
     """
-    return " ".join(sorted(guessed_letters))
-
-
-def build_guessed_word(target_word: str, guessed_letters: Set[str]) -> str:
-    """
-    Build the word to show the player. This takes in the target word and a set of guessed letters. If any of the letters
-    in the target word exist in the guessed letters, the are added to the output. If not, an underscore is added in its
-    place.
-    Args:
-        target_word (str): Target word to build from guessed letters
-        guessed_letters (set): set of guessed letters
-
-    Returns:
-        str: returns the guessed word
-
-    """
-    current_letters = []
-
-    for letter in target_word:
-        if letter in guessed_letters:
-            current_letters.append(letter)
-        else:
-            current_letters.append("_")
-
-    return " ".join(current_letters)
+    config = ConfigParser()
+    config.read(secrets_file)
+    return config["openweather"]["api_key"]
